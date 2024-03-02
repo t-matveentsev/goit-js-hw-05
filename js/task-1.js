faf57bbf1aa214c9493fbd2046f4539491e25aa8
t-matveentsev <tmatveentsev@gmail.com>
2024-03-02 01:44:21 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,14 @@
 'use strict';
+function changeEven(numbers, value) {
+  const newArrea = []
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 === 0) {
+      newArrea.push(numbers[i] + value)
+    } else {
+      newArrea.push(numbers[i])
+    }
+  }
+  return newArrea
+}
+
+console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
