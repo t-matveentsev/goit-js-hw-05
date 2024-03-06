3d32eba7f0ca00ca34d6bd414be25a7fc9f3e4db
t-matveentsev <tmatveentsev@gmail.com>
2024-03-06 17:54:59 +0100

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,44 @@
 'use strict';
-function changeEven(numbers, value) {
-  const newArrea = []
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] % 2 === 0) {
-      newArrea.push(numbers[i] + value)
-    } else {
-      newArrea.push(numbers[i])
-    }
-  }
-  return newArrea
-}
 
-console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+
+const getUserNames = (users) => users.map(user => user.name)
+  
+console.log(
+  getUserNames([
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    balance: 2764
+  },
+])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
